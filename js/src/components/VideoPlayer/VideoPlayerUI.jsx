5f38447dbc9622c0413b064f37536705b7f84277
davlia <danedaworld@gmail.com>
2016-11-12 13:17:25 -0500

    1337 lines added

--- js/src/components/VideoPlayer/VideoPlayerUI.jsx
+++ js/src/components/VideoPlayer/VideoPlayerUI.jsx
@@ -4,15 +4,12 @@ let ProgressBar = require("./ProgressBar");
 
 class VideoPlayerUI extends React.Component {
 
-
   constructor(props) {
     super(props);
   }
 
   componentDidMount() {
-
   }
-
 
   render() {
     return (
@@ -28,20 +25,5 @@ class VideoPlayerUI extends React.Component {
     );
   }
 }
-  //
-  // <div id="player-progress">
-  //   <div ref={(e) => {this.progressBar = e;}} id="player-progress-bar" />
-  //   <div ref={(e) => {this.playerTime = e;}} id="player-time" />
-  // </div>
-  // <div id="left-player-controls">
-  //   <div ref={(e) => {this.playButton = e;}} id="play-button" className="unselectable"/>
-  //   <div id="player-volume">
-  //     <button ref={(e) => {this.volumeButton = e;}} id="player-volume-button" />
-  //     <div ref={(e) => {this.volumeSlider = e;}} id="player-volume-slider" />
-  //   </div>
-  // </div>
-  // <div id="right-player-controls">
-  //   <button ref={(e) => {this.playerSettings = e;}} id="player-settings" />
-  //   <button ref={(e) => {this.fullScreen = e;}} id="full-screen" />
-  // </div>
+
 module.exports = VideoPlayerUI;

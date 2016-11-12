fe9533a6bfc6a9e9fa034b0585b797419707152a
David Liao <danedaworld@gmail.com>
2016-11-12 01:35:18 -0500

    progress bar

---
+++ js/src/components/Room.jsx
@@ -0,0 +1,16 @@
+let React = require("react");
+let Chat = require("./Chat");
+let CanvasVideoPlayer = require("./CanvasVideoPlayer");
+
+class Room extends React.Component {
+  render() {
+    return (
+      <div className="main">
+        <CanvasVideoPlayer websocketAddr={this.props.websocketAddr} roomId={this.props.roomId} />
+        <Chat />
+      </div>
+    );
+  }
+}
+
+module.exports = Room;

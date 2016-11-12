63d59752b58e29226a097c91d8c505d5fb666afe
davlia <danedaworld@gmail.com>
2016-11-12 04:35:21 -0500

    continuous integration

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-export WEB_ADDR=localhost:8000
-export SYNC_ADDR=localhost:8001
+export WEB_ADDR=159.203.88.91:8000
+export SYNC_ADDR=159.203.88.91:8001
 
 ADDR=$WEB_ADDR ./web & PID_LIST+=" $!"
 ADDR=$SYNC_ADDR ./sync-server & PID_LIST+=" $!"

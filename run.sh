f54b795ca9597fa105fa78eb802e9ea07c4943da
Zhan Xiong Chin <czxcjx1234@gmail.com>
2016-11-14 22:32:41 -0500

    Add localhost option for run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
-export WEB_ADDR=159.203.88.91:8000
-export SYNC_ADDR=159.203.88.91:8001
+#!/bin/bash
+
+if [ $LOCALHOST -eq 1 ]; then
+  export WEB_ADDR=localhost:8000;
+  export SYNC_ADDR=localhost:8001;
+else
+  export WEB_ADDR=159.203.88.91:8000;
+  export SYNC_ADDR=159.203.88.91:8001;
+fi
 
 ADDR=$WEB_ADDR ./web & PID_LIST+=" $!"
 ADDR=$SYNC_ADDR ./sync-server & PID_LIST+=" $!"
 npm run webpack-watch & PID_LIST+=" $!"
 
-trap "kill $PID_LIST" INT
+trap "kill $PID_LIST" SIGINT
 
 wait $PID_LIST

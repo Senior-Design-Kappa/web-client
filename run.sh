be05a0b418de97e400fddf1f889d4aeb32812f71
Zhan Xiong Chin <czxcjx1234@gmail.com>
2017-02-21 03:17:06 -0500

    Fix run script

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+if [ $LOCALHOST -eq 1 ]; then
+  export WEB_ADDR=localhost:8000;
+  export SYNC_ADDR=localhost:8001;
+else
+  export WEB_ADDR=159.203.88.91:8000;
+  export SYNC_ADDR=159.203.88.91:8001;
+fi
+
 ADDR=$WEB_ADDR ./web & PID_LIST+=" $!"
 ADDR=$SYNC_ADDR ./sync-server & PID_LIST+=" $!"
 npm run webpack-watch & PID_LIST+=" $!"
